682cedc1847cd39aad42fc8b785e89c13a5e2146
goreleaserbot <goreleaser@carlosbecker.com>
2020-09-13 00:30:56 -0500

    Brew formula update for goad version 0.1.5

--- goad.rb
+++ goad.rb
@@ -2,16 +2,16 @@
 class Goad < Formula
   desc ""
   homepage ""
-  version "0.1.3"
+  version "0.1.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/hmschreck/goad/releases/download/0.1.3/goad_0.1.3_Darwin_x86_64.tar.gz"
-    sha256 "dc600749eaf0374c822216eaed34840df9e05d7253160de6ff0a61af56246d0c"
+    url "https://github.com/hmschreck/goad/releases/download/0.1.5/goad_0.1.5_Darwin_x86_64.tar.gz"
+    sha256 "41851d2764db1c210c321cb9767e4afd4a20ce1599a622da048a6f533a42e6df"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/hmschreck/goad/releases/download/0.1.3/goad_0.1.3_Linux_x86_64.tar.gz"
-      sha256 "385a910f76c6d95f4972c3525d474b0b091fd869a1c919d761c84205afe18a85"
+      url "https://github.com/hmschreck/goad/releases/download/0.1.5/goad_0.1.5_Linux_x86_64.tar.gz"
+      sha256 "8940d2be8156b82c8992d8dfafc4b248c25525c1947039010c5b6ff44c8fc251"
     end
   end
 
